68947df5c7eba1d53d999414fcf0852721b7f4d8
Noel da Costa <noeldacosta@gmail.com>
2024-05-21 01:37:59 +0100

    - better tab styles

--- src/components/molecules/Tabs.svelte
+++ src/components/molecules/Tabs.svelte
@@ -75,9 +75,13 @@
         clip-path: var(--tjs-icon-button-clip-path, none);
         transform-style: preserve-3d;
         width: 100%;
-        height: 100%;
+        height: 200%;
+        margin: -10px 2px;
         font-weight: normal;
-        margin: 0 2px;
+        
+        margin-bottom: -10px;
+        padding: 10px 0;
+        align-items: end;
 
         &:not(:first-child) {
           border-left: none;
@@ -87,17 +91,18 @@
           &:not(:disabled) {
             clip-path: var(--tjs-icon-button-clip-path-hover, var(--tjs-icon-button-clip-path, none));
           }
-          color: var(--player-contrast);
-          box-shadow: none;
+          background: #f9f9f9;
+
         }
 
         &.active {
-          background: var(--player-color);
-          color: var(--player-contrast);
+          background: #f9f9f9;
+
         }
       }
     }
   }
+
 
   .tab-content {
     @include flex-column;
