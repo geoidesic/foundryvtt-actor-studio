7005880cd8b2ecd0a346da954525e76b4ad479cd
Noel da Costa <noeldacosta@gmail.com>
2024-05-23 20:51:05 +0100

    Race selection now shows the race description

--- src/components/organisms/dnd5e/Tabs/Race.svelte
+++ src/components/organisms/dnd5e/Tabs/Race.svelte
@@ -3,6 +3,7 @@ import SvelteSelect from 'svelte-select';
 import IconSelect from '~/src/components/atoms/select/IconSelect.svelte';
 import { extractMapIteratorObjectProperties, getPackFolders, addItemToCharacter, log } from "~/src/helpers/Utility.js";
 import { getContext, onDestroy, onMount } from "svelte";
+import { localize } from "#runtime/svelte/helper";
 
 let active = null, value = null, placeHolder = "Races";
 let pack = game.packs.get('dnd5e.races');
@@ -17,33 +18,31 @@ const actor = getContext("#doc");
 
 $: actorObject = $actor.toObject();
 $: options = raceDefinitions;
+$: html = race?.system?.description.value || '';
 
 log.d('actor', actor);
 log.d('$actor', $actor);
 
 const selectHandler = async (option) => {
-	race = await fromUuid(option)
-	active = option; 
-	log.d('race', race);
+  race = await fromUuid(option)
+  active = option; 
+  log.d('race', race);
+  log.d('race.system.description', race.system.description);
 }
-
-const clickHandler = async () => {
-	await createActorInGameAndEmbedItems();
-}
-
 
 
 /**
  * So the only viable strategy is to keep the race additions in storage 
- * and then only add them after the Actor is added to the game
+ * and then only add them after the Actor is added to the game. 
+ * I haven't found a way to add them to the Actor before it is persisted.
  */
 const createActorInGameAndEmbedItems = async () => {
-	const itemData = race.toObject()
-	log.d('itemData', itemData)
-	const actorInGame = await Actor.create(actorObject);
-	log.d('actorInGame' , actorInGame)
-	const result = await addItemToCharacter(actorInGame, itemData)
-	log.d('result', result);
+  const itemData = race.toObject()
+  log.d('itemData', itemData)
+  const actorInGame = await Actor.create(actorObject);
+  log.d('actorInGame' , actorInGame)
+  const result = await addItemToCharacter(actorInGame, itemData)
+  log.d('result', result);
 }
 
 log.d('folders', folders);
@@ -56,11 +55,12 @@ log.d('raceDefinitions', raceDefinitions);
 <template lang="pug">
 div.tab-content
   .flexrow
-    .flex1.border-right
-      +if("value")
-        button(label="Submit" on:click!="{clickHandler}") Next
+    .flex2.pr-sm
       IconSelect.icon-select({options} {active} {placeHolder} handler="{selectHandler}" id="asdlfkj" bind:value )
-    .flex3
+      h3.left {localize('GAS.Tabs.Races.Select')}
+    .flex0.border-right.right-border-gradient-mask 
+    .flex3.left.pl-md.scroll(bind:innerHTML="{html}" contenteditable)
+  
 </template>
 
 <style lang="scss" scoped>
