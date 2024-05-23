502e7ce2f621588cd239561d793d30daaaf2114b
Noel da Costa <noeldacosta@gmail.com>
2024-05-23 23:56:38 +0100

    #11 - basic spells page pulls journal using `entrichHTML`

--- src/components/organisms/dnd5e/Tabs/Spells.svelte
+++ src/components/organisms/dnd5e/Tabs/Spells.svelte
@@ -1,100 +1,37 @@
 <script>
-import SvelteSelect from 'svelte-select';
-import IconSelect from '~/src/components/atoms/select/IconSelect.svelte';
-import { extractMapIteratorObjectProperties, getPackFolders, addItemToCharacter, log } from "~/src/helpers/Utility.js";
-import { getContext, onDestroy, onMount } from "svelte";
+  import SvelteSelect from 'svelte-select';
+  import { extractMapIteratorObjectProperties, getPackFolders, addItemToCharacter, log, getRules } from "~/src/helpers/Utility";
+  import { getContext, onDestroy, onMount, tick } from "svelte";
+  import Tabs from '~/src/components/molecules/Tabs.svelte';
+  import ManualEntry from '~/src/components/organisms/dnd5e/AbilityEntry/ManualEntry.svelte';
+  import { localize } from "#runtime/svelte/helper";
+  
+  const actor = getContext("#doc");
+  const ruleConfig = { journalId: 'QvPDSUsAiEn3hD8s', pageId: 'evx9TWix4wYU51a5' };
 
-let active = null, value = null, placeHolder = "Races";
-let pack = game.packs.get('dnd5e.races');
-let folders = getPackFolders(pack, 1);
-let folderIds = folders.map(x => x._id);
-let allRaceItems = extractMapIteratorObjectProperties(pack.index.entries(), ['name->label','img', 'type', 'folder', 'uuid->value', '_id']);
-let raceDefinitions = allRaceItems.filter(x => folderIds.includes(x.folder));
-let race; 
+  let rules = '', richHTML = '';
+  
+  $: actorObject = $actor.toObject();
+  $: html = rules?.content || '';
 
-const actor = getContext("#doc");
-
-
-$: actorObject = $actor.toObject();
-$: options = raceDefinitions;
-
-log.d('actor', actor);
-log.d('$actor', $actor);
-
-const selectHandler = async (option) => {
-    race = await fromUuid(option)
-    active = option; 
-    log.d('race', race);
-}
-
-const clickHandler = async () => {
-    await createActorInGameAndEmbedItems();
-}
-
-
-
-/**
- * So the only viable strategy is to keep the race additions in storage 
- * and then only add them after the Actor is added to the game
- */
-const createActorInGameAndEmbedItems = async () => {
-    const itemData = race.toObject()
-    log.d('itemData', itemData)
-    const actorInGame = await Actor.create(actorObject);
-    log.d('actorInGame' , actorInGame)
-    const result = await addItemToCharacter(actorInGame, itemData)
-    log.d('result', result);
-}
-
-log.d('folders', folders);
-log.d('folderIds', folderIds);
-log.d('allRaceItems', allRaceItems);
-log.d('raceDefinitions', raceDefinitions);
-
+  onMount(async () => {
+      rules = await getRules(ruleConfig);
+      await tick();
+      richHTML = await TextEditor.enrichHTML(html);
+  });
+    
 </script>
-
+    
 <template lang="pug">
-div.tab-content
-  .flexrow
-    .flex1.border-right
-      +if("value")
-        button(label="Submit" on:click!="{clickHandler}") Next
-      IconSelect.icon-select({options} {active} {placeHolder} handler="{selectHandler}" id="asdlfkj" bind:value )
-    .flex3
+  div.tab-content
+    .flexrow
+      .flex2.pr-sm.col-a
+      .flex0.border-right.right-border-gradient-mask 
+      .flex3.left.pl-md.scroll.col-b(bind:innerHTML="{richHTML}" contenteditable)
 </template>
 
-<style lang="scss" scoped>
-.tab-content {
-    --tjs-app-overflow: visible;
-  padding: 20px; // Add padding to the tab content
-  border: 2px solid transparent; // Start with a transparent border
-  border-radius: 10px; // Add some rounded corners
-  background-color: #f9f9f9; // Add a light background color
-  position: relative; // Enable absolute positioning for decorations
-
-  // Add decorative elements
-  &:before,
-  &:after {
-    content: "";
-    position: absolute;
-    top: -10px;
-    left: -10px;
-    right: -10px;
-    bottom: -10px;
-    z-index: -1;
-    border: 2px solid #8b0000; // Choose a dark red color for the border
-    border-radius: 15px; // Adjust the radius for a more wicked look
-  }
-
-  &:before {
-    transform: rotate(-3deg); // Rotate the border element for a dynamic effect
-  }
-
-  &:after {
-    transform: rotate(3deg); // Rotate the border element for a dynamic effect
-  }
-  .border-right {
-    border-right: 4px solid brown;
-  }
-}
+<style lang="sass" scoped>
+@import "../../../../../styles/Mixins.scss"
+.tab-content 
+  @include staticOptions
 </style>
