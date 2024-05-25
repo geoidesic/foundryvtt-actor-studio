2cc2ddc20a2aea4b4fca391721d9891ce422bc8f
Noel da Costa <noeldacosta@gmail.com>
2024-05-25 19:00:47 +0100

    #12 - Navigation footer added with a working integration with dnd5e Advancement dialogs

--- src/components/molecules/Footer.svelte
+++ src/components/molecules/Footer.svelte
@@ -1,24 +1,104 @@
 <script>
   import { getContext, onDestroy, onMount, tick } from "svelte";
   import { MODULE_ID } from "~/src/helpers/constants";
+  import { race, characterClass, characterSubClass, background, spells, subRace } from "~/src/helpers/store";
+    import { addItemToCharacter, log } from "~/src/helpers/Utility";
 
   export let value = null;
 
   const actor = getContext("#doc");
+  const app = getContext('#external').application;
 
+  $: actorObject = $actor.toObject();
   $: value = $actor?.name || '';
   $: tokenValue = $actor?.flags?.[MODULE_ID]?.tokenName || value ;
 
   const handleNameInput = (e) => {
     $actor.name = e.target.value;
-    console.log($actor)
+    log.d($actor)
   }
   const handleTokenNameInput = (e) => {
-    console.log(e.target.value);
+    log.d(e.target.value);
     if(!$actor.flags[MODULE_ID]) $actor.flags[MODULE_ID] = {};
     $actor.flags[MODULE_ID].tokenName = e.target.value;
-    console.log($actor)
+    log.d($actor)
   }
+
+  const clickHandler = async () => {
+    await createActorInGameAndEmbedItems();
+  }
+
+
+  /**
+ * So the only viable strategy is to keep the race additions in storage 
+ * and then only add them after the Actor is added to the game
+ */
+const createActorInGameAndEmbedItems = async () => {
+	const actorInGame = await Actor.create(actorObject);
+
+  // background
+  if($background) {
+    log.i('Adding background to character')
+    const backgroundData = $background.toObject()
+    await addItemToCharacter(actorInGame, backgroundData)
+    setTimeout(() => {
+      tick();
+    }, 300);
+  }
+  
+  // spells
+  // if($spells) {
+  //   log.i('Adding spells to character')
+  //   log.i('Adding spells to character')
+  //   const spellsData = $spells.toObject()
+  //   await addItemToCharacter(actorInGame, spellsData)
+  //   setTimeout(() => {
+  //     tick();
+  //   }, 300);
+  // }
+
+  // // race
+  // if($race) {
+  //     log.i('Adding race to character')
+  //     const raceData = $race.toObject()
+  //     await addItemToCharacter(actorInGame, raceData)
+  //     setTimeout(() => {
+  //       tick();
+  //     }, 300);
+  // }
+  
+  // // subrace
+  // if($subRace) {
+  //   log.i('Adding subrace to character')
+  //   const subRaceData = $subRace.toObject()
+  //   await addItemToCharacter(actorInGame, subRaceData)
+  //   setTimeout(() => {
+  //     tick();
+  //   }, 300);
+  // }
+  
+  // // character class
+  // if($characterClass) {
+  //   log.i('Adding class to character')
+  //   const characterClassData = $characterClass.toObject()
+  //   await addItemToCharacter(actorInGame, characterClassData)
+  //   setTimeout(() => {
+  //     tick();
+  //   }, 300);
+  // }
+  
+  // // character subclass
+  // if($characterSubClass) {
+  //   log.i('Adding subclass to character')
+  //   const characterSubClassData = $characterSubClass.toObject()
+  //   await addItemToCharacter(actorInGame, characterSubClassData)
+  //   setTimeout(() => {
+  //     tick();
+  //   }, 300);
+  // }
+
+  // app.close();
+}
 
 </script>
 
@@ -37,7 +117,7 @@
           .flex2
             input.left(type="text" value="{tokenValue}" on:input="{handleTokenNameInput}")
     .flex1
-      button(type="button" role="button") Create
+      button(type="button" role="button" on:mousedown="{clickHandler}") Create
 </template>
 
 
