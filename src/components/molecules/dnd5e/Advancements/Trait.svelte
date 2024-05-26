127f8344630fd54ed3e52a3e72c39caf1d4f9449
Noel da Costa <noeldacosta@gmail.com>
2024-05-26 23:57:57 +0100

    broke out a few more advancements for background

--- src/components/molecules/dnd5e/Advancements/Trait.svelte
+++ src/components/molecules/dnd5e/Advancements/Trait.svelte
@@ -3,6 +3,12 @@ import { getContext, onDestroy, onMount, tick } from "svelte";
 
 export let advancement = null;
 
+$: grantArray = Array.from(advancement.configuration.grants).map(grant => {
+  const [label, value] = grant.split(':');
+  return { label, value };
+});
+
+
 onMount(async () => {
   console.log('advancement'+advancement.type, advancement)
 
@@ -11,9 +17,23 @@ onMount(async () => {
 </script>
 
 <template lang="pug">
-
+.advancement.mt-sm
+  +if("advancement.title === 'Languages'")
+    .flexrow
+      .flex.left
+        span.label Any
+      .flex.right
+        span.value {advancement.configuration.choices[0].count}
+  +if("advancement.title === 'Skills'")
+    +each("grantArray as skill")
+      .flexrow
+        .flex.left {game.system.config.skills[skill.value].label}
+    
 </template>
 
 <style lang="sass">
+  @import "../../../../../styles/Mixins.scss"
+  .advancement
+    @include inset
 
 </style>
