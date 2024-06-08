252ba921bc10259d8c17bb4d827ffdb45456bdcb
Noel da Costa <noeldacosta@gmail.com>
2024-06-08 18:28:22 +0100

    #31 - added point buy costs table data

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -42,4 +42,15 @@ export const MERGE_OPTIONS = {
   inplace: false,
 };
 
+export const POINT_BUY_COSTS = {
+  8: 0,
+  9: 1,
+  10: 2,
+  11: 3,
+  12: 4,
+  13: 5,
+  14: 6,
+  15: 7
+}
+
 export type CLASS_LEVEL = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20;
