f081222e3b24997ce1843650326636feac3f2105
Noel da Costa <noeldacosta@gmail.com>
2024-06-28 00:03:00 +0100

    fix - the Actor Studio button would disappear when an actor was created or moved

--- src/index.js
+++ src/index.js
@@ -60,7 +60,6 @@ Hooks.on('renderAdvancementManager', async (app, html, data) => {
       dropItemRegistry.updateCurrentProcess({ app, html, data })
       const advancementsTab = get(tabs).find(x => x.id === "advancements");
       if (advancementsTab) {
-        log.d('Advancements tab found, rendering it')
         Hooks.call("gas.renderAdvancement");
       } else {
         log.d('Advancements tab not found, adding it to the tabs')
@@ -74,6 +73,8 @@ Hooks.on('renderAdvancementManager', async (app, html, data) => {
 
 Hooks.on('gas.renderAdvancement', () => {
   log.d('gas.renderAdvancement')
+  log.d('Advancements tab found, rendering the advancment workflow')
+
   const currentProcess = get(dropItemRegistry.currentProcess);
   // Get all stored advancement apps
   if (currentProcess) {
@@ -97,7 +98,7 @@ Hooks.on('dnd5e.preAdvancementManagerComplete', (...args) => {
 
 Hooks.on('closeAdvancementManager', async (...args) => {
   // Define a function to check if the panel is empty
-  const isPanelEmpty = () => $('#foundryvtt-actor-studio-pc-sheet .window-content main section.a .tab-content .content').html().trim() === '';
+  const isPanelEmpty = () => $('#foundryvtt-actor-studio-pc-sheet .window-content main section.a .tab-content .container .content')?.html()?.trim() === '';
 
   // Define a function to wait for the panel to become empty
   const waitForPanelEmpty = async () => {
@@ -113,6 +114,7 @@ Hooks.on('closeAdvancementManager', async (...args) => {
   
   // Once the panel is empty, proceed with the queue
   const queue = await dropItemRegistry.advanceQueue();
+  log.d('closeAdvancementManager queue', queue)
   if (!queue) {
     Hooks.call("gas.close");
   }
@@ -197,88 +199,82 @@ function isActorTypeValid(actorTypes, type) {
   return actorTypes.hasOwnProperty(type) && actorTypes[type] === true;
 }
 
-function getActorStudioButton() {
+function getActorStudioButton(buttonId) {
   const gasButton = $(
-    `<button id="gas-button" type="button" class='dialog-button default bright' data-gas_start style="display: flex; align-items: center; justify-content: center; background-color: white; padding: 0; margin: 0; height: 40px;">
+    `<button id="${buttonId}" type="button" class='dialog-button default bright' data-gas_start style="display: flex; align-items: center; justify-content: center; background-color: white; padding: 0; margin: 0; height: 40px;">
       <img src="modules/${MODULE_ID}/assets/actor-studio-blue.svg" alt="Actor Studio" style="height: 100%; max-height: 30px; border: none; width: auto;">
     </button>`,
   );
   return gasButton;
 }
 
-function addCreateNewActorButton(html, app) {
-  log.i('Adding Create New Actor button');
-  const select = $('select', html);
-  const systemActorDocumentTypes = dnd5e.actorTypes
 
-  function updateButton() {
-    const actorType = select.val();
-    log.d('actorType', actorType)
-    if (isActorTypeValid(systemActorDocumentTypes, actorType)) {
-      if (!$('button[data-gas_start]', html).length) {
-        const $gasButton = getActorStudioButton();
-        log.d('html', html)
-        $('button', html).last().after($gasButton); // Ensure button is added after the Create New Actor confirm button
-
-        const handleButtonClick = function (e) {
-          if (e.type === 'mousedown' || e.type === 'keydown' && (e.key === 'Enter' || e.key === ' ')) {
-            if (userHasRightPermissions()) {
-              const actorName = $('input', html).val();
-              const folderName = $('select[name="folder"]', html).val();
-              log.d('actorType', actorType);
-              try {
-                log.d('app', app)
-                log.d('app.getData()', app.getData())
-                // new PCApplication(new Actor.implementation({ name: actorName, flags: { [MODULE_ID]: {folderName}}, type: actorType })).render(true, { focus: true });
-                new PCApplication(new Actor.implementation({ name: actorName, folder: folderName, type: actorType })).render(true, { focus: true });
-                app.close();
-              } catch (error) {
-                ui.notifications.error(error.message);
-              }
-            }
-          }
-        };
-
-        $gasButton.on('mousedown', handleButtonClick);
-        $gasButton.on('keydown', handleButtonClick);
-
-      }
-    } else {
-      $('button[data-gas_start]', html).remove(); // Remove button if actorType is not "character"
-    }
-  }
-
-  // Initial check
-  updateButton();
-
-  // Update button when the select value changes
-  select.on('change', updateButton);
-}
 
 Hooks.on('renderApplication', (app, html, data) => {
-
   const createNewActorLocalized = game.i18n.format('DOCUMENT.Create', { type: game.i18n.localize('DOCUMENT.Actor') });
   if (app.title === createNewActorLocalized) {
-    addCreateNewActorButton(html, app);
+    log.i('Adding Create New Actor button');
+
+    const select = $('select', html);
+    const systemActorDocumentTypes = dnd5e.actorTypes
+
+    function updateButton() {
+      const actorType = select.val();
+      log.d('actorType', actorType)
+      if (isActorTypeValid(systemActorDocumentTypes, actorType)) {
+        if (!$('#gas-dialog-button', html).length) {
+          const $gasButton = getActorStudioButton('gas-dialog-button');
+          log.d('html', html)
+          $('button', html).last().after($gasButton); // Ensure button is added after the Create New Actor confirm button
+
+          const handleButtonClick = function (e) {
+            if (e.type === 'mousedown' || e.type === 'keydown' && (e.key === 'Enter' || e.key === ' ')) {
+              if (userHasRightPermissions()) {
+                const actorName = $('input', html).val();
+                const folderName = $('select[name="folder"]', html).val();
+                log.d('actorType', actorType);
+                try {
+                  // new PCApplication(new Actor.implementation({ name: actorName, flags: { [MODULE_ID]: {folderName}}, type: actorType })).render(true, { focus: true });
+                  new PCApplication(new Actor.implementation({ name: actorName, folder: folderName, type: actorType })).render(true, { focus: true });
+                  app.close();
+                } catch (error) {
+                  ui.notifications.error(error.message);
+                }
+              }
+            }
+          };
+
+          $gasButton.on('mousedown', handleButtonClick);
+          $gasButton.on('keydown', handleButtonClick);
+
+        }
+      } else {
+        $('#gas-dialog-button', html).remove(); // Remove button if actorType is not "character"
+      }
+    }
+
+    // Initial check
+    updateButton();
+
+    // Update button when the select value changes
+    select.on('change', updateButton);
   }
 })
 
 
-Hooks.on('changeSidebarTab', async (app) => {
+Hooks.on('renderActorDirectory', async (app) => {
   if (!game.modules.get(MODULE_ID)?.active) return;
   // Add Actor Studio button to the sidebar
   if (app.constructor.name === "ActorDirectory") {
     if(!game.settings.get(MODULE_ID, 'showButtonInSideBar')) return;
-    if($('#gas-button').length) return;
-    const $gasButton = getActorStudioButton();
+    if($('#gas-sidebar-button').length) return;
+    const $gasButton = getActorStudioButton('gas-sidebar-button');
     $(app._element).find('header.directory-header').append($gasButton);
 
     const handleButtonClick = function (e) {
       if (e.type === 'mousedown' || e.type === 'keydown' && (e.key === 'Enter' || e.key === ' ')) {
         if (userHasRightPermissions()) {
           try {
-            log.d('app', app)
-            log.d('app.getData()', app.getData())
             // new PCApplication(new Actor.implementation({ name: actorName, flags: { [MODULE_ID]: {folderName}}, type: actorType })).render(true, { focus: true });
             new PCApplication(new Actor.implementation({ name: game.user.name, folder: '', type: 'character' })).render(true, { focus: true });
             app.close();
