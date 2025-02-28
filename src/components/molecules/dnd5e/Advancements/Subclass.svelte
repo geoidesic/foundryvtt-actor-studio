077aa899524333729119f1319181b6a3290b68ad
Noel da Costa <noeldacosta@gmail.com>
2025-02-28 19:38:01 +0000

    #120 - subclasses are advancements in dnd5e v4.x, - thus the Subclass rendering component is needed for showing advancements. - This is currently an empty placeholder

---
+++ src/components/molecules/dnd5e/Advancements/Subclass.svelte
@@ -0,0 +1,12 @@
+<script>
+import { getContext, onMount } from "svelte";
+
+onMount(async () => {
+  console.log('Subclass Advancements');
+});
+</script>
+<template lang="pug">
+
+</template>
+<style lang="sass">
+</style>
