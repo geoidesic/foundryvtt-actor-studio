95f406d4f4b10befe1328259baefe89f9ff85dcd
Noel da Costa <noeldacosta@gmail.com>
2025-03-02 03:13:00 +0000

    added ImageButton

---
+++ src/components/atoms/button/ImageButton.svelte
@@ -0,0 +1,28 @@
+<script>
+  import preventDefault from "~/src/helpers/svelte-actions/PreventDefault.js";
+
+  export let image = void 0;
+  export let alt = "";
+  export let disabled = false;
+</script>
+
+<button on:click on:mousedown={preventDefault} {disabled}>
+  <img src={image} alt={alt} />
+</button>
+
+<style lang="scss">
+  @import "../../../../styles/Mixins.scss";
+  button {
+    @include icon-button;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+    clip-path: var(--tjs-icon-button-clip-path, none);
+    transform-style: preserve-3d;
+  }
+
+  button:hover {
+    background: radial-gradient(var(--button-background-highlight), var(--button-background));
+    clip-path: var(--tjs-icon-button-clip-path-hover, var(--tjs-icon-button-clip-path, none));
+  }
+</style>
