462a0e7aef56ec5a90a481a8d0580d76590edad2
Noel da Costa <noeldacosta@gmail.com>
2025-06-11 12:17:50 +0100

    refactor: moved export

--- src/helpers/AdvancementManager.js
+++ src/helpers/AdvancementManager.js
@@ -171,3 +171,20 @@ export class AdvancementManager {
     return await this.watchAdvancementManager();
   }
 } 
+
+
+// Function to remove all elements with the specified class
+export const destroyAdvancementManagers = () => {
+    // Select all elements with the class 'application dnd5e2 advancement manager'
+    const elements = document.querySelectorAll('.application.dnd5e2.advancement.manager');
+    console.log('advancement managers', elements);
+    // Iterate over the NodeList and remove each element
+    elements.forEach(element => {
+        element.remove(); // Remove the element from the DOM
+    });
+}
+
+export default {
+  AdvancementManager,
+  destroyAdvancementManagers
+};
