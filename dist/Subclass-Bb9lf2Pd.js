991f059bb8fb290f4069b6e8671bdb7ec07379c6
Noel da Costa <noeldacosta@gmail.com>
2025-09-18 14:58:42 +0100

    chore: build and bump version to 2.0.5

---
+++ dist/Subclass-Bb9lf2Pd.js
@@ -0,0 +1,17 @@
+import { S as SvelteComponent, i as init, s as safe_not_equal, o as onMount } from "./index-BJ1f51Mm.js";
+function instance($$self) {
+  onMount(async () => {
+    console.log("Subclass Advancements");
+  });
+  return [];
+}
+class Subclass extends SvelteComponent {
+  constructor(options) {
+    super();
+    init(this, options, instance, null, safe_not_equal, {});
+  }
+}
+export {
+  Subclass as default
+};
+//# sourceMappingURL=Subclass-Bb9lf2Pd.js.map
--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,2 @@
-import "./index-zEOjuOYs.js";
+import "./index-BJ1f51Mm.js";
 //# sourceMappingURL=index.js.map
